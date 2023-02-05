1a5dea3f21614c479f8788314d20f2f98df7c2b1
SamFlint <sam.flint@hyperluminalgames.com>
2023-02-05 11:51:07 +0000

    Added finalised planting animation

--- Roots/Assets/Roots/Scripts/Runtime/Grid/GridObject.cs
+++ Roots/Assets/Roots/Scripts/Runtime/Grid/GridObject.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using MoreMountains.Feedbacks;
+using System.Collections;
 
 public class GridObject : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class GridObject : MonoBehaviour
     private GameObject _occupied;
     [SerializeField]
     private MMF_Player _player;
+    [SerializeField]
+    private GameObject[] _seeds;
+    [SerializeField]
+    private GameObject _seedParent;
 
 
     private void Start()
@@ -16,11 +21,37 @@ public class GridObject : MonoBehaviour
         _player.Initialization();
     }
 
-    public void SetOccupied(bool occupied)
+    public void SetOccupied(bool occupied, int vegType)
     {
-        _unoccupied.SetActive(!occupied);
-        _occupied.SetActive(occupied);
+        for (int i = 0; i < _seeds.Length; i++)
+        {
+            _seeds[i].SetActive(false);
+            if (i == vegType)
+            {
+                _seeds[i].SetActive(true);
+            }
+        }
+
+        if (occupied)
+        {
+            StartCoroutine(modelSwap());
+        }
+        else
+        {
+            _unoccupied.SetActive(!occupied);
+            _occupied.SetActive(occupied);
+            _player.PlayFeedbacks();
+        }
+    }
+
+    IEnumerator modelSwap()
+    {
+        _unoccupied.SetActive(true);
+        _seedParent.SetActive(true);
+        yield return new WaitForSeconds(0.75f);
+        _occupied.SetActive(true);
+        _unoccupied.SetActive(false);
+        _seedParent.SetActive(false);
         _player.PlayFeedbacks();
     }
-    
 }
